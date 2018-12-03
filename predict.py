72d59d9bf7276098d886c3235f7db8de9faec660
Alexander Rodionov <alx1dr@gmail.com>
2018-12-03 17:59:39 +0300

    upd

--- predict.py
+++ predict.py
@@ -32,17 +32,3 @@ else:
     print('pass shop and item to predict sales')
     print('you can pass file or 2 numbers')
     sys.exit(1)
-
-
-
-if len(sys.argv) == 2:   # from file
-    print()
-elif len(sys.argv) == 3: # from args
-    shop = int(sys.argv[1])
-    item = int(sys.argv[2])
-    key = (shop, item)
-    print(predict_by_key(key))
-else:
-    print('pass shop and item to predict sales')
-    print('you can pass file or 2 numbers')
-    sys.exit(1)
