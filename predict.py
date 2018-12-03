99c7a1dc50f95c1d99bfa86ad6dfc4c662b64456
Alexander Rodionov <alx1dr@gmail.com>
2018-12-03 17:59:09 +0300

    upd

--- predict.py
+++ predict.py
@@ -1,8 +1,8 @@
 import pickle
 import sys
 
-with open('models.pickle', 'rb') as handle:
-    models = pickle.load(handle)
+# with open('models.pickle', 'rb') as handle:
+#     models = pickle.load(handle)
 
 
 def predict_by_key(key):
@@ -11,12 +11,32 @@ def predict_by_key(key):
     else:
         return str(key) + ' not in models list'
 
+
+
 if len(sys.argv) == 2:   # from file
-    with open(sys.argv[1]) as f:
-        for line in f:
-            s = line.split()
-            key = (int(s[0]), int(s[1]))
+    if sys.argv[1] == '-a':
+        for key in models.keys():
             print(predict_by_key(key))
+    else:
+        with open(sys.argv[1]) as f:
+            for line in f:
+                s = line.split()
+                key = (int(s[0]), int(s[1]))
+                print(predict_by_key(key))
+elif len(sys.argv) == 3: # from args
+    shop = int(sys.argv[1])
+    item = int(sys.argv[2])
+    key = (shop, item)
+    print(predict_by_key(key))
+else:
+    print('pass shop and item to predict sales')
+    print('you can pass file or 2 numbers')
+    sys.exit(1)
+
+
+
+if len(sys.argv) == 2:   # from file
+    print()
 elif len(sys.argv) == 3: # from args
     shop = int(sys.argv[1])
     item = int(sys.argv[2])
