a3015110156c70c8da87f6dbbaacdd01660a234c
Alexander Rodionov <alx1dr@gmail.com>
2018-12-04 12:11:23 +0300

    upd

--- print-prediction.py
+++ print-prediction.py
@@ -29,30 +29,30 @@ elif args.item:
 else:
     result = df
 
-# print('='*72)
-# print('\n'*5)
+report  = ''
+report += '='*72
+report += '\n'*5
+
 for row in result.collect():
-    print('shop', row.shop, 'item', row.item)
-    print('sales prediction for next 4 weeks:')
+    report += 'shop' + str(row.shop) + 'item' + str(row.item) + '\n'
+    report += 'sales prediction for next 4 weeks:\n'
 
     for week in range(4):
-        print('week', week, end=' ')
+        report += 'week' + str(week) + ' '
         week_pred = row.prediction[week * 7 : (week + 1) * 7]
         week_pred_round = [round(p) for p in week_pred]
         
         for p in week_pred_round:
-            print(str(p).rjust(3), end=' ')
-        print('week sum:', sum(week_pred_round))
-    print()
+            report += str(p).rjust(3) + ' '
+        report += 'week sum:' + str(sum(week_pred_round))
+    report += '\n'
     # rounded = [round(p) for p in row.prediction]
     # rounded_str = []
     # print(row.shop, row.item, end=' ')
     # for p in row.prediction:
         # print('{r:3}'.format(r=round(p)), end=' ')
     # print()
-# print('\n'*5)
-# print('='*72)
+report += '\n'*5
+report += '='*72
 
-
-# TODO
-# add sum for each W1 W2 W3 W4 in report
+print(report)
