a719cc6ee5ecd1d4e13463039d7272f256f53d25
Alexander Rodionov <alx1dr@gmail.com>
2018-12-04 11:51:14 +0300

    upd

--- print-prediction.py
+++ print-prediction.py
@@ -33,10 +33,26 @@ else:
     result = df
 
 for row in result.collect():
-    print(row.shop, row.item, end=' ')
-    for p in row.prediction:
-        print('{r:3}'.format(r=round(p)), end=' ')
-    print()
+    print('shop', row.shop, 'item', item)
+
+    print('sales prediction for next 4 weeks:')
+
+    for week in range(4):
+        print('week', week, end=' ')
+        week_pred = row.prediction[week * 7 : (week + 1) * 7]
+        week_pred_round = [round(p) for p in week_pred]
+        
+        for p in week_pred_round:
+            print(str(p).rjust(3), end=' ')
+        print('week sum:', sum(week_pred_round))
+
+
+    # rounded = [round(p) for p in row.prediction]
+    # rounded_str = []
+    # print(row.shop, row.item, end=' ')
+    # for p in row.prediction:
+        # print('{r:3}'.format(r=round(p)), end=' ')
+    # print()
 
 
 # TODO
